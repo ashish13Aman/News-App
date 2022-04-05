bb2f2f745303538ca69efb8998a1af95bc3e3b04
james-anmol <jamesannmol27@gmail.com>
2022-04-05 20:58:07 +0530

    done...:)

---
+++ app/src/main/java/com/example/NewsApp/splashScreen.java
@@ -0,0 +1,30 @@
+package com.example.NewsApp;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class splashScreen extends AppCompatActivity {
+
+    @RequiresApi(api = Build.VERSION_CODES.P)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+      new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(splashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();;
+            }
+        },2000);
+    }
+}
